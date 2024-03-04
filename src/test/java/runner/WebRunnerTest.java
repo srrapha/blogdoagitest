0caa820fff60b86e867836ffa4d92b68e5bb2d4d
Raphael <sribeiro.rapha@gmail.com>
2024-03-04 01:36:33 -0300

    adicionando a classe executora do cucumber

---
+++ src/test/java/runner/WebRunnerTest.java
@@ -0,0 +1,21 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "steps.web",
+        tags = "@CenarioComum",
+        plugin = {
+                "pretty",
+                "html:reports/cucumber.html",
+                "json:reports/cucumber.json",
+                "junit:reports/cucumber.xml"
+        }
+)
+public class WebRunnerTest {
+
+}
